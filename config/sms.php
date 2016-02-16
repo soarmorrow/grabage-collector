64eca2cde16596e34996f7a16937d712a2313882
Vineeth Krishnan <vineethkrishnan007@gmail.com>
2016-02-16 16:06:13 +0530

    Initial commit with app structure

--- config/sms.php
+++ config/sms.php
@@ -10,7 +10,7 @@
  * Time: 8:36 PM
  */
 return [
-    'url' => 'http://host6.hemsmedia.com/app/smsapi/index.php',
-    'api_key' => '55559991134CE7',
-    'sender_id' => 'OPAUTO'
+    'url' => 'http://api.sms.com',
+    'api_key' => 'XXXXXXXXXXXXXXXXXXX',
+    'sender_id' => 'XXXXXX'
 ];
